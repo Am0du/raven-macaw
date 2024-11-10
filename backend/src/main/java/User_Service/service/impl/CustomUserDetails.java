009d14d5130a02be529d616b00bc3943d15a7381
Amodu <amodudavid777@gmail.com>
2024-11-10 16:01:05 +0100

    feat: added customUser details to load user from db

---
+++ backend/src/main/java/User_Service/service/impl/CustomUserDetails.java
@@ -0,0 +1,21 @@
+package User_Service.service.impl;
+
+import User_Service.exception.BadRequestException;
+import User_Service.exception.NotFoundException;
+import User_Service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new BadRequestException("Invalid credentials"));
+    }
+}
