111f9360471c0b1e6a807d3056e61d7f14bc62cb
Amodu <amodudavid777@gmail.com>
2024-11-10 16:03:24 +0100

    feat: swagger confi

---
+++ backend/src/main/java/User_Service/config/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package User_Service.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi(){
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User Service").version("1.0")
+                        .description("A user service api"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        ))
+                .security(Collections.singletonList(
+                        new SecurityRequirement().addList("bearerAuth")
+                ));
+    }
+}
